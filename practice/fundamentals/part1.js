34463a72c32238e1a0dd06c2e867f642a8fb2158
KazukiTakeuchi <takeuchi1227813813@gmail.com>
2022-06-15 21:33:47 +0900

    javascript fundamentals part1

--- practice/fundamentals/part1.js
+++ practice/fundamentals/part1.js
@@ -1,1 +1,12 @@
-console.log('read test');
+// calculate BMI
+const massMark = 78;
+const heightMark = 1.69;
+
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
