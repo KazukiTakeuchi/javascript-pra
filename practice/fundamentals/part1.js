7a339348b04347810e70b5ccbcdbe6dd4739c87a
KazukiTakeuchi <takeuchi1227813813@gmail.com>
2022-06-15 21:44:38 +0900

    ``で囲むことで下記の書き方で文字が結合できる ${name}

--- practice/fundamentals/part1.js
+++ practice/fundamentals/part1.js
@@ -10,3 +10,15 @@ const BMIJohn = massJohn / (heightJohn * heightJohn);
 const markHigherBMI = BMIMark > BMIJohn;
 
 console.log(BMIMark, BMIJohn, markHigherBMI);
+
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + 'years old' + job + '!';
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(jonasNew);
