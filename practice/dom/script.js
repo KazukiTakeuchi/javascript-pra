8f2dc5657c07f9be7aa840294ee10d1c96ab7ee6
KazukiTakeuchi <takeuchi1227813813@gmail.com>
2022-07-03 11:01:56 +0900

    入力された数値によってテキストが変わるようにした

--- practice/dom/script.js
+++ practice/dom/script.js
@@ -10,9 +10,9 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
-const x = function() {
-    console.log(23)
-}
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
     const guess = Number(document.querySelector('.guess').value);
@@ -20,5 +20,19 @@ document.querySelector('.check').addEventListener('click', function () {
 
     if (!guess) {
         document.querySelector('.message').textContent = `No number!`;
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = `🎉 Correct Number`;
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = `Too hight`;
+        score--;
+        document.querySelector('.score').textContent = score;
+    } else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = `Too low`;
+        score--;
+        document.querySelector('.score').textContent = score;
+    }
+
+    if (score < 1) {
+        document.querySelector('.message').textContent = `You lost the game`
     }
 });
