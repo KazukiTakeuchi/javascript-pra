4e6a889e573b021b49ff42a00720843956442fb9
KazukiTakeuchi <takeuchi1227813813@gmail.com>
2022-07-03 11:02:42 +0900

    不要なコメントアウトの削除

--- practice/dom/script.js
+++ practice/dom/script.js
@@ -1,15 +1,5 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = `🎉 Correct Number`;
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-
-// console.log(document.querySelector('.guess').value);
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
-
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 document.querySelector('.number').textContent = secretNumber;
