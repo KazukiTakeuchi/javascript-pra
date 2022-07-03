71552c9ec247a208a7014f09480f4b926efc2a85
KazukiTakeuchi <takeuchi1227813813@gmail.com>
2022-07-03 11:25:59 +0900

    問題の数値が表示されないようにし、正解時のみ開示するようにした

--- practice/dom/script.js
+++ practice/dom/script.js
@@ -2,7 +2,6 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 10;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', ()=> {
     const guess = Number(document.querySelector('.guess').value);
@@ -12,6 +11,7 @@ document.querySelector('.check').addEventListener('click', ()=> {
         document.querySelector('.message').textContent = `No number!`;
     } else if (guess === secretNumber) {
         document.querySelector('.message').textContent = `🎉 Correct Number`;
+        document.querySelector('.number').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = '#60b347';
 
         document.querySelector('.number').style.width = '30rem';
@@ -35,6 +35,8 @@ document.querySelector('.again').addEventListener('click', ()=> {
     score = 10;
     secretNumber = Math.trunc(Math.random() * 20) + 1;
     document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
     document.querySelector('body').style.backgroundColor = '#222';
     document.querySelector('.number').style.width = '15rem';
 });
